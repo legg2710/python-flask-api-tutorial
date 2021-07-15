e6ce07810eaec76a943ebff3c5b9dd47a05ca2e2
legg2710 <luisemilio2710@gmail.com>
2021-07-15 21:42:18 +0000

    python flask api tutorial 49%

--- src/app.py
+++ src/app.py
@@ -1,5 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/todos', methods=['GET'])
+def hello_world():
+
+    todos = {"label": "task1", "done": False}
+
+    json_text = flask.jsonify(todos)
+    return json_text
+
+    todos = {"label": "task1", "done": False}
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
