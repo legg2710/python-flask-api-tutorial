ef19339fcd6a0499483a821521ce52e2f67a45d4
legg2710 <luisemilio2710@gmail.com>
2021-07-16 21:30:42 +0000

    python flask api 90%

--- src/app.py
+++ src/app.py
@@ -1,17 +1,27 @@
 import flask
 import json
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 todos = [
-    { "label": "Task1", "done": False },
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False },
 ]
+
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
     global todos
-    json_text = flask.jsonify(todos)
+    json_text=flask.jsonify(request_body)
+    decoded_object = json.loads(todos)
     return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data.decode
+    print("Incoming request with the following body", request_body)
+    return 'Response for the POST todo'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
